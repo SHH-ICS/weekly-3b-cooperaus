b4e2c35e793e90f952574248d45a300db66d93e2
cooperaus <144241777+cooperaus@users.noreply.github.com>
2023-11-02 09:28:28 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,5 +1,12 @@
 1T=1, 2T=1.75, 3T=2.5, 4T=3.35, L=6, XL=10
-L/XL+1T/2T/3T/4T=ANS
+L+1T=ANS
+L+2T=ANS
+L+3T=ANS
+L+4T=ANS
+XL+1T=ANS
+XL+2T=ANS
+XL+3T=ANS
+XL+4T=ANS
 ANS*1.13=TOTAL
 print"enter variable"
 print(TOTAL)
